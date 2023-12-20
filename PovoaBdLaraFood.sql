f5c6dc29fa9e19297c22aa644903e71ad03e58cf
Gabriel Coelho <gcoelho730@gmail.com>
2023-12-19 23:42:12 -0300

    aula_03: cap_02

---
+++ PovoaBdLaraFood.sql
@@ -0,0 +1,4 @@
+insert into plans(name, url, price, description)
+values
+	('Plano-01', 'plano-01', 24.99, 'Apenas um plano legal'),
+	('Plano Prime', 'plano-prime', 85.99, 'Um plano otimo');
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,10 @@
 |
 */
 
+use App\Http\Controllers\Admin\PlanController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function() {
     return view('welcome');
